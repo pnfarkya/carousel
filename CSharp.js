3d19c3b0d227b2a37834c28808158c43eea15092
pnfarkya <31060653+pnfarkya@users.noreply.github.com>
2023-08-02 18:06:31 +0530

    Update CSharp.js

--- CSharp.js
+++ CSharp.js
@@ -1,1 +1,22 @@
 
+Collection in C#
+  Generic collection      Non-Generic Collection     Concurrent Collection
+      List                ArrayList                  ConcurrentBag
+      Hashset                                        ConcurrentDictionary
+      Dictionary          Hashtable
+      Queue               Queue                      ConcurrentQueue
+      Stack               Stack                      ConcurrentStack
+      SortedList                                     OrderablePartitioner
+
+
+Advantages/Dis of Genecric Collection
+- No boxing unboxing requires 
+- Faster
+- Easy to perform Linq quries 
+- Type saftey
+
+Advantages/Dis of Genecric Collection
+- Boxing unboxing slow down the collection
+- hard to perform Linq
+- Useful for multiple type data coming from external soures like search query 
+-
